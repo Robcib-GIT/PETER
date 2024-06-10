e1d85004c01fd60e87b402694ef5c2fe056bb35d
jgsamartin <jgsamartin@gmail.com>
2024-06-10 10:47:36 +0200

    Circle detection not working

--- Code_python/Tools/red-circle/measure-length.py
+++ Code_python/Tools/red-circle/measure-length.py
@@ -1,13 +1,15 @@
 """
-    Capture video from the default camera and detect a red circle in the image.
+    Read a video or a picture and detect a red circle in each frame.
     Jorge F. García-Samartín
     www.gsamartin.es
-    2024-05-23
+    2024-05-31
 """
 
 DEBUG = True
 
 import os
+import cv2
+import numpy as np
 from DetectCircles import detect_circles
 
 def main():
@@ -15,8 +17,20 @@ def main():
     # Get current working directory
     currDir = os.path.dirname(os.path.abspath(__file__)) + "/"
 
-    # Detect the red circles in the video stream
-    detected_circles = detect_circles(currDir + "videos/short.mp4")
+    # Load video or image
+    video = currDir + "videos/short.mp4"
+    cap = cv2.VideoCapture(video)
+    #ret, captured_frame = cap.read()
+
+    # Load all the frames of the video
+    frames = []
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+
+    # For each frame in the video, map to detected_circles
 
     # Show the number of detected circles in each frame
     if DEBUG:
